a2f577a71c621f7f39fde69905d14674333dd288
LavedaNafisArshanda <42015136+LavedaNafisArshanda@users.noreply.github.com>
2019-04-09 07:48:53 +0700

    Add files via upload

--- search.php
+++ search.php
@@ -1,75 +1,52 @@
-<?php 
-   
-   include '../connect.php';
-   $cari = $_GET['cari'];
-   $kategori = $_GET['kategori'];
+<?php
 
-    $query = "SELECT kode, nama_matkul, sks, semester, id_dosen, nama_dosen
-            FROM matakuliah LEFT JOIN dosen
-            USING (id_dosen)
-            WHERE $kategori LIKE '%$cari%'
-            ORDER BY (kode)";
+include '../connect.php';
+$cari = $_GET['cari'];
 
-    $result = mysqli_query($connect, $query);
-
-    $num = mysqli_num_rows($result);
+$query = "SELECT * FROM dosen WHERE nama_dosen LIKE '%$cari%'";
+$result = mysqli_query($connect, $query);
+$num = mysqli_num_rows($result);
 
 ?>
 
-
-
-
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <table border="1">
-            <tr>
-                <th>No.</th>
-                <th>Kode</th>
-                <th>Matakuliah</th>
-                <th>SKS</th>
-                <th>Semester</th>
-                <th>Dosen Pengajar</th>
-                <th>Aksi</th>
-            </tr>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+    <table border='1'>
+    <h2>Data Dosen</h2>
+    <tr>
+        <th>No.</th>
+        <th>Nama Dosen</th>
+        <th>Telepon</th>
+        <th>Aksi</th>
+        <th>Delete</th>
+    </tr>
 
-            <?php 
-                if($num > 0) 
-                {
-                    $no = 1;
-                    while ($data = mysqli_fetch_assoc($result)) { ?>
-                        
-                        <tr>
-                            <td> <?php echo $no; ?> </td>
-                            <td> <?php echo $data['kode'] ?> </td>
-                            <td> <?php echo $data['nama_matkul'] ?> </td>
-                            <td> <?php echo $data['sks'] ?> </td>
-                            <td> <?php echo $data['semester'] ?> </td>
-                            <td>  
-                                <?php
-                                    if($data[ 'nama_dosen'] != NULL )
-                                    { echo $data['nama_dosen']; }
-                                    else { echo "-"; }
-                                ?>
-                            </td>
-                            <td>
-                                <a href="form-update.php?kode=<?php echo $data['kode']; ?>">Edit </a>
-                                <a href="delete.php?kode=<?php echo $data['kode']; ?>" onclick="return confirm('Anda yakin ingin menghapus data?')"> Hapus </a>
-                            </td>
-                        </tr>
+    <?php
+        if($num > 0)
+        {
+            $no = 1;
+            while($data = mysqli_fetch_assoc($result))
+            {
+                echo "<tr>";
+                echo "<td>" . $no . "</td>";
+                echo "<td>" . $data['nama_dosen'] . "</td>";
+                echo "<td>" . $data['telp'] . "</td>";
+                echo "<td><a href='Form-Update.php?id_dosen=" . $data['id_dosen'] . "'>Edit</a> | ";
+                echo "<td><a href='Delete.php?id_dosen=" . $data['id_dosen'] . "'onclick='return confirm(\"Apakah Anda Yakin Ingin Menghapus Data ?\")' >Hapus</a></td>";              
+                echo "</tr>";
+                $no++;
+            }
+        }
+        else
+        {
+            echo "<td colspan='3'> Tidak Ada Data</td>";
+        }
+    ?>
 
-                    <?php
-                    $no++;
-                    }
-                } 
-                else 
-                {
-                    echo "<tr> <td colspan='7'>Tidak ada data</td></tr>";
-                }
-            ?>
-        </table>
-    </body>
+    </table>
+</body>
 </html>
